7efa80a92cf2ae66780a7732088babec84b678fb
Daniele Barsotti <sixtynine.db@gmail.com>
2021-02-03 11:03:39 +0100

    Get rid of Helper/Arrays

--- src/SixtyNine/Timesheep/Config.php
+++ src/SixtyNine/Timesheep/Config.php
@@ -15,34 +15,34 @@ class Config
     {
         $dateFormat = getenv('DATE_FORMAT');
         $timeFormat = getenv('TIME_FORMAT');
-        Arrays::setValueDotted($this->config, 'db.url', getenv('TIMESHEEP_DB_URL'));
-        Arrays::setValueDotted($this->config, 'console.box-style', getenv('BOX_STYLE') ?: 'default');
-        Arrays::setValueDotted($this->config, 'time.rounding', getenv('TIME_ROUNDING') ?: '5');
-        Arrays::setValueDotted($this->config, 'format.date', $dateFormat ?: 'd-m-Y');
-        Arrays::setValueDotted($this->config, 'format.time', $timeFormat ?: 'H:i');
-        Arrays::setValueDotted(
-            $this->config,
-            'format.datetime',
-            $dateFormat && $timeFormat ? $dateFormat.' '.$timeFormat: 'd-m-Y H:i'
-        );
+        $this->config = [
+            'db' => [
+                'url' => getenv('TIMESHEEP_DB_URL'),
+            ],
+            'console' => [
+                'box-style' => getenv('BOX_STYLE') ?: 'default',
+            ],
+            'time' => [
+                'rounding' => getenv('TIME_ROUNDING') ?: '5',
+            ],
+            'format' => [
+                'date' => $dateFormat ?: 'd-m-Y',
+                'time' => $timeFormat ?: 'H:i',
+                'datetime' => $dateFormat && $timeFormat ? $dateFormat.' '.$timeFormat: 'd-m-Y H:i',
+            ],
+        ];
     }
 
     public function get(string $key): string
     {
-        return Arrays::getValueDotted($this->config, $key) ?? '';
-    }
-
-    public function toArray(): array
-    {
-        return [
-            'db' => ['url' => $this->get('db.url')],
-            'console' => ['box-style' => $this->get('console.box-style')],
-            'time' => ['rounding' => $this->get('time.rounding')],
-            'format' => [
-                'date' => $this->get('format.date'),
-                'time' => $this->get('format.time'),
-                'datetime' => $this->get('format.datetime'),
-            ]
-        ];
+        $indexes = explode('.', $key);
+        $curItem = $this->config;
+        foreach ($indexes as $idx) {
+            if (!isset($curItem[$idx])) {
+                return '';
+            }
+            $curItem = $curItem[$idx];
+        }
+        return $curItem;
     }
 }
