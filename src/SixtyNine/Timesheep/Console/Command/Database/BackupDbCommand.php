1220157eb975214958e22145b0698a9e8ff658da
Daniele Barsotti <d.barsotti@kreuzfahrtberater.ch>
2022-04-25 10:51:24 +0200

    Implement db:backup

--- src/SixtyNine/Timesheep/Console/Command/Database/BackupDbCommand.php
+++ src/SixtyNine/Timesheep/Console/Command/Database/BackupDbCommand.php
@@ -6,6 +6,8 @@ use SixtyNine\Timesheep\Console\TimesheepCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Process\Process;
 
 class BackupDbCommand extends TimesheepCommand
 {
@@ -21,6 +23,28 @@ class BackupDbCommand extends TimesheepCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        throw new \Exception('Not implemented');
+        $io = new SymfonyStyle($input, $output);
+
+        $outPath = $input->getArgument('output-file');
+
+        $dbParams = $this->em->getConnection()->getParams();
+        $path = $dbParams['path'];
+
+        if (!file_exists($path)) {
+            throw new \InvalidArgumentException('Cannot find DB file: ' . $path);
+        }
+
+        if (file_exists($outPath) && !is_writable($outPath)) {
+            throw new \InvalidArgumentException('The output file is not writable: ' . $outPath);
+        }
+
+        $process = new Process(['cp', $path, $outPath]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $io->error('Error while copying the database: ' . $process->getErrorOutput());
+        } else {
+            $io->writeln('Database copied to ' . $outPath);
+        }
     }
 }
